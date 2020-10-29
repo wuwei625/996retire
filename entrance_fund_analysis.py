2f424ab4bec4ee77b79cfc51653f6295ed39e715
wuwei <wuwei@aifin.ltd>
2020-10-29 12:41:22 +0800

    基金池扩容及更新

--- entrance_fund_analysis.py
+++ entrance_fund_analysis.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     fund_code, fund_nav = internal_questionnaire.fund_analysis()
     while not util_func.isExitCode(fund_code):
         if fund_code == "list":
-            output_fund.show_list()
+            # 此处括号中可以加入参数，参数是收益基准值（日），用(年利率+1)^(1/244)-1获取
+            output_fund.show_list(0.000572)   
         else:
             output_fund.show_single_fund_mean_and_var(fund_code, fund_nav)
         fund_code, fund_nav = internal_questionnaire.fund_analysis()
